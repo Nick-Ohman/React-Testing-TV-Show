4c1c331dae9492c7c53a66bbd1e00163b4aa96a8
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-05-18 16:58:28 -0500

    struggle bus

---
+++ src/App.test.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { render, queryAllByTestId, getByText, fireEvent } from '@testing-library/react';
+import { fetchShow as mockFetchShow } from './api/fetchShow';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+
+
+const showData = {
+
+    name: "Stranger Things",
+    image: {
+        original: "http://static.tvmaze.com/uploads/images/original_untouched/200/501942.jpg"
+    },
+    summary: "<p>A love letter to the '80s classics that captivated a generation, <b>Stranger Things<\/b> is set in 1983 Indiana, where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl.<\/p>",
+    _embedded: {
+        episodes: [
+            {
+                id: 2993,
+                name: "Chapter One: The Vanishing of Will Byers",
+                season: 1,
+                number: 1,
+                runtime: 60,
+                image: {
+                    medium: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+                },
+                summary: "<p>A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.<\/p>",
+            },
+            {
+                id: 553946,
+                name: "Chapter One: MADMAX",
+                season: 2,
+                number: 1,
+                runtime: 60,
+                image: {
+                    medium: "http://static.tvmaze.com/uploads/images/medium_landscape/132/331976.jpg",
+                },
+                summary: "<p>One year after the events with the Upside Down and the Demogorgon, Will meets with a government doctor. The boys discover that there's a new player in town, and Jim pays a visit to El.<\/p>",
+            },
+            {
+                id: 578663,
+                name: "Chapter One: Suzie, Do You Copy?",
+                season: 3,
+                number: 1,
+                runtime: 51,
+                image: {
+                    medium: "http://static.tvmaze.com/uploads/images/medium_landscape/204/510098.jpg",
+                },
+                summary: "<p>Things change over the summer: Jonathan, Nancy, Steve, and Billy get jobs; Dustin goes to science camp; El and Mike become an item; Lucas and Max almost become an item. Meanwhile, mysterious power outages plague Hawkins and rats start exploding.<\/p>",
+
+            },
+            {
+                id: 1752754,
+                name: "Chapter One: The Hellfire Club",
+                season: 4,
+                number: 1,
+                runtime: 60,
+                image: null,
+                summary: null,
+
+            }
+        ]
+    }
+}
+
+test("renders without errors", () => {
+    mockFetchShow.mockResolveValueOnce(showData);
+    render(<App />);
+})
+
+jest.mock('./api/fetchShow');
+test("renders data after api is called", async () => {
+    mockFetchShow.mockResolveValueOnce(showData);
+    const { queryByText } = render(<App />);
+    await waitFor(() => {
+        const selection = getByText(/Select a season/i)
+        fireEvent.click(selection)
+
+    }
+    );
+
+    expect(queryByText(/chapter one/i)).toHaveLength(4)
+    expect(mockFetchShow).toHaveBeenCalled(1);
+});
+
